f5224d00ff75c1db0bbd23ea512a6cb133533a63
emondewan00 <dewanmohammademon@gmail.com>
2023-06-14 17:38:58 +0600

    create enrroled modale

--- routeHandler/enrroledHandler.js
+++ routeHandler/enrroledHandler.js
@@ -3,3 +3,5 @@ const mongoose = require("mongoose");
 const veryfyJwt = require("../middleware/veryfyJwt");
 const enrroledSchema = require("../schemas/enrroledSchema");
 const router = express.Router();
+
+const Enrroled = new mongoose.model("Enrroled", enrroledSchema);
