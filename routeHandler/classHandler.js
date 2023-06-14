9fc4afaba53d5aae4d48665ea539644dbea9ca3b
emondewan00 <dewanmohammademon@gmail.com>
2023-06-14 17:45:07 +0600

    create class modale

--- routeHandler/classHandler.js
+++ routeHandler/classHandler.js
@@ -4,3 +4,8 @@ const userSchema = require("../schemas/userSchema");
 const mongoose = require("mongoose");
 const router = express.Router();
 const veryfyJwt = require("../middleware/veryfyJwt");
+
+//class model
+
+const Class = new mongoose.model("Class", classSchema);
+const User = new mongoose.model("User", userSchema);
