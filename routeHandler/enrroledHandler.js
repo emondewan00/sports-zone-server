7b61c625de6021914f3d1dd48f7f0e606a9b6ecf
emondewan00 <dewanmohammademon@gmail.com>
2023-06-14 17:38:21 +0600

    create enrroled handler

---
+++ routeHandler/enrroledHandler.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const veryfyJwt = require("../middleware/veryfyJwt");
+const enrroledSchema = require("../schemas/enrroledSchema");
+const router = express.Router();
