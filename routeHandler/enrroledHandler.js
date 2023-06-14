604dec28bbfa83137bff427dfdc32de73832337e
emondewan00 <dewanmohammademon@gmail.com>
2023-06-14 17:48:31 +0600

    create  get enrroled class route

--- routeHandler/enrroledHandler.js
+++ routeHandler/enrroledHandler.js
@@ -5,3 +5,18 @@ const enrroledSchema = require("../schemas/enrroledSchema");
 const router = express.Router();
 
 const Enrroled = new mongoose.model("Enrroled", enrroledSchema);
+
+// get enrroled class
+
+router.get("/:userId", veryfyJwt, async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const result = await Enrroled.find({ userId }).populate("classId");
+    res.send(result);
+  } catch (error) {
+    console.log(error)
+    res.send(error);
+  }
+});
+
+module.exports = router;
