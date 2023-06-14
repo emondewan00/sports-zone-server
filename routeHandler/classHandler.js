de2cbf8d925ab0ed3709f7d63aaa0915a8e12fbd
emondewan00 <dewanmohammademon@gmail.com>
2023-06-14 17:45:34 +0600

    get all classes and aprove pending reject classes

--- routeHandler/classHandler.js
+++ routeHandler/classHandler.js
@@ -9,3 +9,14 @@ const veryfyJwt = require("../middleware/veryfyJwt");
 
 const Class = new mongoose.model("Class", classSchema);
 const User = new mongoose.model("User", userSchema);
+
+//get all classes and aprove pending reject classes
+router.get("/", async (req, res) => {
+  const status = req.query.status || { $in: ["pending", "aprove", "reject"] };
+  try {
+    const classes = await Class.find({ status });
+    res.status(200).json(classes);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch classes" });
+  }
+});
